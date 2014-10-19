21683b943d6073e09d46d3c8ab6cb5902161d492
Maurice <sabertoothmaurice@gmail.com>
2014-10-19 17:25:32 +0530

    unsuccessful trials with watch

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,30 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, Game) {
+.controller('DashCtrl', function($scope, Game, Data) {
+//        console.log("RELOAD");
     $scope.allusers = Game.allUsers();
+    $scope.debug = Game.debug();
+    $scope.data = Data.data;
+//    $scope.$watch(
+//        function(){ return Data.data },
+//        function(newVal) {
+//            console.log("CALLED WATCH");
+//            $scope.data = newVal;
+//        }
+//    );
+   $scope.refresh = function(){
+//       $scope.data = Data.data;
+       $scope.allusers = Game.allUsers();
+       $scope.debug = Game.debug();
+       $scope.data = Data.data;
+       console.log("BUTTON:REFRESH");
+   }
+    Game.addController($scope.refresh);
 })
 
 .controller('FriendsCtrl', function($scope, Game) {
   $scope.dataApi = Game.friends;
-  setTimeout(function(){Game.add(); console.log("ADDED");},1500);
+//  setTimeout(function(){Game.add(); console.log("ADDED");},1500);
 
 })
 
--- www/js/services.js
+++ www/js/services.js
@@ -4,16 +4,25 @@ var users = [];
 var dataApi ={
     users : users
 }
+var refreshControllers = [];
+var debug ="nothing";
 $(document ).ready(function() {
     console.log( "APP ready" );
-    socket = io('http://192.168.1.3:9000', {path: '/socket.io'});
+    socket = io('http://192.168.1.10:9000', {path: '/socket.io'});
     socket.emit('all users', 'hi');
     socket.on('all users', function(msg){
         console.log("ALL USERS RECEIVED:"+msg.toString());
+        dataApi.users.splice(0,dataApi.users.length);
+//        console.log("received users:"+JSON.stringify(dataApi.users));
         msg.forEach(function(userId){
             dataApi.users.push({ id : userId });
         });
-        console.log("received users:"+dataApi.users.toString());
+        console.log("received users:"+JSON.stringify(dataApi.users));
+        dataApi.debug+=JSON.stringify(dataApi.users);
+        for(var i=0;i<refreshControllers.length;i++){
+            console.log("REFRESHING STACK"+i);
+            refreshControllers[i]();
+        }
 
 
     });
@@ -28,6 +37,7 @@ angular.module('starter.services', [])
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
+  this.debug2 = "FIRST";
   var friends = [
     { id: 0, name: 'Scruff McGruff' },
     { id: 1, name: 'G.I. Joe' },
@@ -35,7 +45,9 @@ angular.module('starter.services', [])
     { id: 3, name: 'Ash Ketchum' }
   ];
 
+
         dataApi.friends = friends;
+        dataApi.debug = debug;
 
   return {
     add: function() {
@@ -53,12 +65,56 @@ angular.module('starter.services', [])
       return socket;
     },
     allUsers: function() {
-//        console.log("users:"+users.toString());
-      return users;
+        console.log("users:"+users.toString());
+      return dataApi.users;
     },
     dataApi: function() {
         console.log("users:"+JSON.stringify(dataApi));
       return dataApi;
+    },
+    debug:function() {
+        console.log("DATAAPI:"+JSON.stringify(dataApi));
+        return dataApi.debug;
+    },
+    addController:function(func) {
+        refreshControllers.push(func)
+      return dataApi.debug;
     }
   }
-});
+})
+
+.service('Data', [function() {
+    this.data = "FIRST";
+    var context = this;
+    this.weatherChange = function() {
+        console.log(this.data);
+        this.data = "SECOND";
+        console.log(this.data);
+    };
+//    setTimeout(function(){context.weatherChange()},2500);
+}])
+.service("Monster", [function(){
+
+    this.alive = false;
+        var context = this;
+//
+    this.weatherChange2 = function(context) {
+        console.log("WEATHER CHAGED");
+        context.alive = true;
+    };
+
+    setTimeout(function(){context.weatherChange2(context)},2500);
+
+}])
+.controller("MadScientistCtrl", ["$scope", "Monster", function($scope, Monster) {
+        $scope.alive = Monster.alive;
+
+        $scope.$watch(
+            function(){ return Monster.alive },
+
+            function(newVal) {
+                console.log('asfsagsgdhdfddddddffffffffffffff');
+                $scope.alive = newVal;
+            }
+        )
+}]);
